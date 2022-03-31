dfea5ae9513d2e32f3a9a0a6bfd60a2cbe699efb
rifki ahmad sururi <rifkisururi27@gmail.com>
2022-03-31 10:48:17 +0700

    change .gitignore

--- app/Config/App.php
+++ app/Config/App.php
@@ -23,7 +23,8 @@ class App extends BaseConfig
      *
      * @var string
      */
-    public $baseURL = 'https://nurulhuda.herokuapp.com/';
+    public $baseURL = 'http://localhost:81/nurulHudaApps/public/';
+    //public $baseURL = 'https://nurulhuda.herokuapp.com/';
 
     /**
      * --------------------------------------------------------------------------
