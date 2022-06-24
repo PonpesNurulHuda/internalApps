ad0275a4b56283b60e229343a82035afcfbac80f
rifki ahmad sururi <rifkisururi27@gmail.com>
2022-06-24 19:27:18 +0700

    title page login

--- app/Views/login.php
+++ app/Views/login.php
@@ -5,7 +5,7 @@
   <meta charset="utf-8">
   <meta content="width=device-width, initial-scale=1.0" name="viewport">
 
-  <title>Pages / Login - NiceAdmin Bootstrap Template</title>
+  <title>Pusat Data Nurul Huda</title>
   <meta content="" name="description">
   <meta content="" name="keywords">
 
@@ -42,9 +42,9 @@
             <div class="col-lg-4 col-md-6 d-flex flex-column align-items-center justify-content-center">
 
               <div class="d-flex justify-content-center py-4">
-                <a href="index.html" class="logo d-flex align-items-center w-auto">
+                <a href="" class="logo d-flex align-items-center w-auto">
                   <img src="assets/img/logo.png" alt="">
-                  <span class="d-none d-lg-block">EnHa</span>
+                  <span class="d-none d-lg-block">Pusat Data Nurul Huda</span>
                 </a>
               </div><!-- End Logo -->
 
