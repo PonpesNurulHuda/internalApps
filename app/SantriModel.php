178cde4e5416ae954934a87e9120cb08d1e52a40
rifki ahmad sururi <rifkisururi27@gmail.com>
2022-06-24 18:40:55 +0700

    test wf

---
+++ app/SantriModel.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Models;
+
+use CodeIgniter\Model;
+use Illuminate\Support\Facades\DB;
+
+class santriModel extends Model
+{
+    protected $table      = 'santri';
+    protected $primaryKey = 'id';
+
+    protected $useAutoIncrement = true;
+    protected $allowedFields = ['kk', 'nik', 'nis', 'nama', 'tanggal_lahir', 'gender', 'is_mustahiq', 'no_hp1', 'no_hp2' ];
+
+    public function GetSantriKelas()
+    {
+        $db      = \Config\Database::connect();
+        $builder = $db->table('santri');
+        $builder->select('santri.id, santri.nama, kelas.id as kelasId');
+        $builder->join('siswa_kelas', 'santri.id = siswa_kelas.id_siswa', 'left');
+        $builder->join('kelas', 'kelas.id = siswa_kelas.id_kelas', 'left');
+        $query = $builder->get()->getResultArray();
+
+        return $query;
+    }
+
+    public function GetSantriKelasByKelas($idKelas)
+    {
+        $db      = \Config\Database::connect();
+        $builder = $db->table('santri');
+        $builder->select('santri.id');
+        $builder->join('siswa_kelas', 'santri.id = siswa_kelas.id_siswa', 'left');
+        $builder->where('siswa_kelas.id_kelas', $idKelas);
+        $query = $builder->get()->getResultArray();
+
+        return $query;
+    }
+
+    public function getJabatanSantri($idSantri)
+    {
+        $db      = \Config\Database::connect();
+        $builder = $db->table('santri');
+        $builder->select('santri.id, jabatan.id as jabatan');
+        $builder->join('santri_jabatan sj', 'sj.id_santri = santri.id', 'left');
+        $builder->where('santri.id', $idSantri);
+        $query = $builder->get()->getResultArray();
+
+        return $query;
+    }
+}
---
+++ app/db.sql
@@ -0,0 +1,115 @@
+CREATE TABLE `kelas` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `kode` varchar(10) NOT NULL,
+  `nama` varchar(20) NOT NULL,
+  `tingkat_id` int(11) NOT NULL,
+  `tahun_ajaran_id` int(11) NOT NULL,
+  `walikelas` varchar(20) NOT NULL,
+  `is_active` int(11) NOT NULL,
+  `created_at` datetime NOT NULL,
+  `updated_at` datetime NOT NULL,
+  PRIMARY KEY (`id`)
+);
+
+CREATE TABLE `mapel` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nama` varchar(20) NOT NULL,
+  `deskripsi` varchar(200) NOT NULL,
+  `mapel_kategori_id` int(11) NOT NULL,
+  `mapel_type` int(11) NOT NULL,
+  `is_active` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+);
+
+CREATE TABLE `mapel_kategori` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nama` varchar(20) NOT NULL,
+  `deskripsi` varchar(200) NOT NULL,
+  `is_active` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+);
+
+CREATE TABLE `mapel_kelas` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nama` varchar(20) NOT NULL,
+  `kelas_id` int(11) NOT NULL,
+  `semester_id` int(11) NOT NULL,
+  `mapel_id` int(11) NOT NULL,
+  `mustahiq` varchar(20) NOT NULL,
+  `keterangan` varchar(200) NOT NULL,
+  PRIMARY KEY (`id`)
+);
+
+CREATE TABLE `mapel_tipe` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nama` varchar(20) NOT NULL,
+  `deskripsi` varchar(200) NOT NULL,
+  `is_active` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+);
+
+CREATE TABLE `nilai_akhlaq_santri` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id_santri` int(11) NOT NULL,
+  `is_semester` int(11) NOT NULL,
+  `akhlaq` int(11) NOT NULL,
+  `kerapihan` int(11) NOT NULL,
+  `kerajinan` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+);
+
+CREATE TABLE `nilai_santri` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id_siswa_kelas` int(11) NOT NULL,
+  `id_mapel_kelas` int(11) NOT NULL,
+  `nilai` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+);
+
+CREATE TABLE `santri` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `kk` varchar(20) NOT NULL,
+  `nik` char(16) NOT NULL,
+  `nis` varchar(10) NOT NULL,
+  `nama` varchar(50) NOT NULL,
+  `tanggal_lahir` date NOT NULL,
+  `gender` char(1) NOT NULL,
+  PRIMARY KEY (`id`)
+);
+
+CREATE TABLE `semester` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `tahun_ajaran_id` int(11) NOT NULL,
+  `seqno` int(11) NOT NULL,
+  `nama` varchar(20) NOT NULL,
+  `dimulai` date NOT NULL,
+  `selesai` date NOT NULL,
+  `status` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+);
+
+CREATE TABLE `siswa_kelas` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id_siswa` int(11) NOT NULL,
+  `id_kelas` int(11) NOT NULL,
+  `id_active` int(11) NOT NULL,
+  `created_at` int(11) NOT NULL,
+  `updated_at` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+);
+
+CREATE TABLE `tahun_ajaran` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nama` varchar(20) NOT NULL,
+  `status` int(11) NOT NULL,
+  `is_active` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+);
+
+CREATE TABLE `tbl01` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `seqno` int(11) NOT NULL,
+  `nama` varchar(20) NOT NULL,
+  `is_active` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+);
