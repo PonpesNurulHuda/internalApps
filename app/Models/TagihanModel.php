d7f1aa94f11ad90ab540d9378ff24d7c6748295e
rifki ahmad sururi <rifkisururi27@gmail.com>
2022-06-27 10:27:21 +0700

    add periode on dashboard

--- app/Models/TagihanModel.php
+++ app/Models/TagihanModel.php
@@ -24,32 +24,37 @@ class TagihanModel extends Model
     public function rekap(){
         $db      = \Config\Database::connect();
         $query = "
-                    select a.*, b.H,b.H/(a.L+b.H) * 100 as persen, b.jumlah from (
+                select a.id, a.nama, a.L, concat(MONTHNAME(b.tanggal_jatuh_tempo), year(b.tanggal_jatuh_tempo)) as Periode, b.H,b.H/(a.L+b.H) * 100 as persen, b.jumlah from (
+                    select 
+                        t.id,
+                        t.nama,
+                        COUNT(tdL.id) as L,
+                        tdL.tanggal_jatuh_tempo
+                    from 
+                        tagihan t
+                        left join tagihan_detail tdL on t.id = tdL.id_tagihan and tdL.status = 1
+                    group by 
+                        t.id,
+                        t.nama,
+                        tdL.tanggal_jatuh_tempo
+                    ) a 
+                    
+                    inner join (
                         select 
                             t.id,
                             t.nama,
-                            COUNT(tdL.id) as L
+                            COUNT(tdL.id) as H,
+                            sum(tdL.jumlah) as jumlah,
+                            tdL.tanggal_jatuh_tempo
                         from 
                             tagihan t
-                            left join tagihan_detail tdL on t.id = tdL.id_tagihan and tdL.status = 1
+                            left join tagihan_detail tdL on t.id = tdL.id_tagihan and tdL.status = 0
                         group by 
                             t.id,
-                            t.nama
-                        ) a 
-                        inner join (
-                            select 
-                                t.id,
-                                t.nama,
-                                COUNT(tdL.id) as H,
-                                sum(tdL.jumlah) as jumlah
-                            from 
-                                tagihan t
-                                left join tagihan_detail tdL on t.id = tdL.id_tagihan and tdL.status = 0
-                            group by 
-                                t.id,
-                                t.nama
-                        ) b on a.id = b.id
-                    where b.H > 0
+                            t.nama,
+                            tdL.tanggal_jatuh_tempo
+                    ) b on a.id = b.id
+                where b.H > 0
         ";
         $query=$this->db->query($query);
         return $query->getResultArray();
